6cd42b125757df08aa0d814ebdc6f77c6ff938af
mecozma <mecozma@gmail.com>
2019-05-08 19:00:25 +0100

    Cleaned up the code and added the first event listeners

--- app.js
+++ app.js
@@ -1,25 +1,30 @@
+// BUDGET CONTROLLER
 var budgetController = (function() {
-  var x = 23;
-  var add = function(a) {
-    return 23 + a;
-  }
-  return {
-    publicTest: function(b) {
-      return add(b);
-    }
-  }
+ 
 })();
 
+// UI CONTROLLER
 var UIController = (function() {
 
 })();
 
+// GLOBALL APP CONTROLLER
 var controller = (function(budgetCtrl, UICtrl) {
-  var z = budgetCtrl.publicTest(7);
-  return {
-    otherFunction: function() {
-      console.log(z);
-    }
+  var ctrlAddItem = function(){
+    // 1. Get the fildd input data;
+    // 2. Add the item to the budget controller;
+    // 3. Add the item to the UI;
+    // 4. Calculate the budget;
+    // 5. Display the budget on the UI;
+    console.log('It works!');
   }
+  document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+
+  document.addEventListener('keypress', function(event) {
+    // event.which is used on older browsers, as they dont heve the .keyCode property
+    if(event.keyCode === 13 || event.which === 13) {
+      ctrlAddItem();
+    }
+  });
 
 })(budgetController, UIController);
